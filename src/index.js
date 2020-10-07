83adf7872b790a8600aa0afc5d3b4b7a1f4aeac7
Nicolás Macias <51766256+Zeitgeist-J@users.noreply.github.com>
2020-10-06 20:28:08 -0500

    Deleted repeated browser

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
